5b33f569a4b8d9a3dba06a224d5e5fc655af2f1f
Hung Duong Quang <qhungg289@protonmail.com>
2022-01-15 23:23:15 +0700

    Update Message model

--- models/Message.js
+++ models/Message.js
@@ -4,7 +4,12 @@ const { DateTime } = require("luxon");
 const messageSchema = new mongoose.Schema({
 	author: { type: mongoose.Types.ObjectId, required: true },
 	content: { type: String, required: true },
-	createdAt: { type: String, default: DateTime.now },
+	createdAt: {
+		type: String,
+		default: () => {
+			DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+		},
+	},
 });
 
 messageSchema.virtual("url").get(function () {
