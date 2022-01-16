1ba81585ffbbf7f486f9260917ca69bc9a63cc27
Hung Duong Quang <qhungg289@protonmail.com>
2022-01-16 23:36:08 +0700

    Update index routes

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get("/signup", indexController.signUpGet);
 
 router.post("/signup", indexController.signUpPost);
 
+router.get("/login", indexController.logInGet);
+
+router.post("/login", indexController.logInPost);
+
+router.get("/logout", indexController.logOutGet);
+
 module.exports = router;
