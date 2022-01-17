9f1e327f7d26297aed4d0bdd9313ad17aa75472a
Hung Duong Quang <qhungg289@protonmail.com>
2022-01-17 22:06:32 +0700

    Add middleware to check request auth status

--- routes/message.js
+++ routes/message.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const messageController = require("../controllers/messageController");
 
+router.use("/new", (req, res, next) => {
+	if (!req.isAuthenticated()) {
+		return res.redirect("/login");
+	}
+	next();
+});
+
 router.get("/new", messageController.newMessageGet);
 
 router.post("/new", messageController.newMessagePost);
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 
+router.use((req, res, next) => {
+	if (!req.isAuthenticated()) {
+		return res.redirect("/login");
+	}
+	next();
+});
+
 router.get("/:id", userController.userDetailGet);
+
+router.get("/:id/member", userController.userMemberGet);
+
+router.post("/:id/member", userController.userMemberPost);
 
 module.exports = router;
