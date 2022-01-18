fa612705bfc2b6b0f1c751e4c0345a856c8069e8
Hung Duong Quang <qhungg289@protonmail.com>
2022-01-18 19:55:19 +0700

    Add get member controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,9 @@
 const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
 
 exports.userDetailGet = async (req, res, next) => {
 	try {
-		if (!req.isAuthenticated()) {
-			return res.redirect("/login");
-		}
-
-		let isLogIn = req.user && req.user._id == req.params.id;
+		const isLogIn = req.user && req.user._id == req.params.id;
 
 		const user = await User.findById(req.params.id).populate("messages");
 
@@ -19,3 +16,44 @@ exports.userDetailGet = async (req, res, next) => {
 		return next(error);
 	}
 };
+
+exports.userMemberGet = (req, res, next) => {
+	if (!(req.user && req.user._id == req.params.id)) {
+		return res.redirect("/login");
+	}
+
+	res.render("user-member", {
+		title: `Only Fun | Become a member`,
+		errors: [],
+	});
+};
+
+exports.userMemberPost = [
+	check("secret").exists().trim().escape(),
+	async (req, res, next) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.render("user-member", {
+				title: `Only Fun | Become a member`,
+				errors: errors.array(),
+			});
+		}
+
+		try {
+			const user = await User.findById(req.params.id);
+
+			if (req.body.secret == "theodinproject") {
+				user.isMember = true;
+				await user.save().then(res.redirect(user.url));
+			} else {
+				res.render("user-member", {
+					title: `Only Fun | Become a member`,
+					errors: [{ msg: "Secret key is incorrect" }],
+				});
+			}
+		} catch (error) {
+			return next(error);
+		}
+	},
+];
